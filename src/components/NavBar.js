b91f2755391cff4c7f7046902e2aef04004d0891
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2022-12-16 05:47:52 +0800

    feat: added app.css and edited social media icons and main logo

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,9 +1,8 @@
 import { useState, useEffect } from "react";
 import { Navbar, Nav, Container } from "react-bootstrap";
-import logo from '../assets/img/logo.svg';
+import logo from '../assets/img/logo.png';
 import navIcon1 from '../assets/img/nav-icon1.svg';
 import navIcon2 from '../assets/img/nav-icon2.svg';
-import navIcon3 from '../assets/img/nav-icon3.svg';
 
 export const NavBar = () => {
 
@@ -29,16 +28,16 @@ export const NavBar = () => {
     }
 
     return (
-        <Navbar expand="lg" className={scrolled ? "scrolled" : ""}>
+        <Navbar expand="md" className={scrolled ? "scrolled" : ""}>
             <Container>
-                <Navbar.Brand href="#home">
-                    <img src={logo} alt="logo" />
+                <Navbar.Brand href="/">
+                    <img src={logo} alt="Logo" />
                 </Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav">
                     <span className="navbar-toggler-icon"></span>
                 </Navbar.Toggle>
                 <Navbar.Collapse id="basic-navbar-nav">
-                <Nav className="me-auto">
+                <Nav className="ms-auto">
                     <Nav.Link href="#home" className={activeLink === 'home' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('home')}>Home</Nav.Link>
                     <Nav.Link href="#skills" className={activeLink === 'skills' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('skills')}>Skills</Nav.Link>
                     <Nav.Link href="#projects" className={activeLink === 'projects' ? 'active navbar-link' : 'navbar-link'} onClick={() => onUpdateActiveLink('projects')}>Projects</Nav.Link>
@@ -46,9 +45,8 @@ export const NavBar = () => {
                 <span className="navbar-text">
                     <div className="social-icon">
                         {/* to do: add social media links*/}
-                        <a href="#"><img src={navIcon1} alt="" /></a>
-                        <a href="#"><img src={navIcon2} alt="" /></a>
-                        <a href="#"><img src={navIcon3} alt="" /></a>
+                        <a href="https://www.linkedin.com/in/xavier-koo/" target="_blank"><img src={navIcon1} alt="" /></a>
+                        <a href="https://github.com/xavierkoo" target="_blank"><img src={navIcon2} alt="" /></a>
                     </div>
                     <button className="vvd" onClick={() => console.log('connect')}><span>Let's Connect</span></button>
                 </span>
