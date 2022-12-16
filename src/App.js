16226564fda8af0517dec241640e39aa48a5d453
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2022-12-17 04:06:09 +0800

    feat: added custom cursor

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import { CursorProvider } from "react-cursor-custom";
 import { NavBar } from "./components/NavBar";
-
+// ADD CUSTOM CURSOR
 function App() {
   return (
     <div className="App">
-      <NavBar />
+     <CursorProvider
+        color={'grey'}
+        ringSize={25}
+        transitionTime={75}
+      >
+        <NavBar />
+      </CursorProvider>
     </div>
   );
 }
