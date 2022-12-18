5c81a7bfb12b6e05244b6f7a5d2b426aab618f5b
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2022-12-19 03:15:57 +0800

    feat: added resume pdf and updated tab image and text

--- src/App.js
+++ src/App.js
@@ -3,7 +3,8 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import withSplashScreen from "./components/withSplashScreen";
 import { CursorProvider } from "react-cursor-custom";
 import { NavBar } from "./components/NavBar";
-import { Banner } from "./components/Banner"
+import { Banner } from "./components/Banner";
+// import { Skills } from "./components/Skills"
 
 function App() {
   return (
