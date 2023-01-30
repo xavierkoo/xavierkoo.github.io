519835abbfee94f5aa14ea8022294c2d2be0bb0e
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-01-30 22:40:46 +0800

    add alt attribute to colorsharp2

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -140,7 +140,7 @@ export const Projects = () => {
                     </Col>
                 </Row>
             </Container>
-            <img className="background-image-right" src={colorSharp2}></img>
+            <img className="background-image-right" src={colorSharp2} alt="background-img2"></img>
         </section>
     )
 }
