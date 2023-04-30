4afadc2bf961e297d443ba7f99deebd2d18c1126
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-05-01 01:02:12 +0800

    feat: add medium blog description

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -72,7 +72,10 @@ export const Experience = () => {
                                     <Typography  fontWeight="bold" variant="h5" component="span">
                                     Technical Writer <Typography variant="h5" color="secondary">@Medium Blog</Typography>
                                     </Typography>
-                                        <Typography></Typography> {/* TODO: add description in list */}
+                                        <Typography>
+                                            Authored a technical blog on software engineering, producing 33 articles covering topics such as data structures &
+                                            algorithms, technologies & frameworks, and essential skills which received 2600 views and 178 followers to date.        
+                                        </Typography>
                                     </TimelineContent>
                                 </TimelineItem>
                                 <TimelineItem>
