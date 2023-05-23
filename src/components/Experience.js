08ba9ebce1d9c3d6555b7c200327537007c2cf1a
BobzBanana <xavier.koo.2021@scis.smu.edu.sg>
2023-05-23 23:42:43 +0800

    feat: update experience

--- src/components/Experience.js
+++ src/components/Experience.js
@@ -10,104 +10,107 @@ import CodeIcon from '@mui/icons-material/Code';
 import Typography from "@mui/material/Typography";
 
 export const Experience = () => {
+    const isMobile = window.matchMedia('(max-width: 768px)').matches;
     
-      return (
-        <section className="experiences" id="experiences">
-            <Container>
-                <Row>
-                    <Col>
-                        <div>
-                        <h2 className="m-5" style={{ fontSize: '45px', fontWeight: '700', textAlign: 'center' }}>
-                            Experiences
-                        </h2>
-                            <Timeline 
-                                sx={{
-                                    [`& .${timelineItemClasses.root}:before`]: {
-                                        flex: 0,
-                                        padding: 0,
-                                    },
-                                }}
-                            >
-                                <TimelineItem>
-                                    <TimelineSeparator>
-                                        <TimelineConnector  />
-                                        <TimelineDot color="secondary">
-                                            <CodeIcon />
-                                        </TimelineDot>
-                                        <TimelineConnector />
-                                    </TimelineSeparator>
-                                    <TimelineContent sx={{ py: "15px", px: 2 }}>
-                                    <Typography variant="p" component="span">
-                                        Feb 2023 - Present
-                                    </Typography> <br />
-                                    <Typography  fontWeight="bold" variant="h5" component="span">
-                                        Software Engineer <Typography variant="h5" color="secondary">@Asterisk Technologies</Typography>
-                                    </Typography>
-                                    <Typography></Typography> {/* TODO: add description in list */}
-                                    <div className="item-exp mt-1">
-                                        <i class="devicon-bootstrap-plain colored"></i>
-                                        <i class="devicon-sass-plain colored"></i>
-                                        <i class="devicon-react-original colored"></i>
-                                        <i class="devicon-express-original"></i>
-                                        <i class="devicon-nodejs-plain colored"></i>
-                                        <i class="devicon-electron-original colored"></i>
-                                        <i class="devicon-jest-plain colored"></i>
-                                        <i class="devicon-mongodb-plain colored"></i>
-                                    </div>
-                                    </TimelineContent>
-                                </TimelineItem>
-                                <TimelineItem>
-                                    <TimelineSeparator>
-                                        <TimelineConnector  />
-                                        <TimelineDot color="secondary">
-                                            <CodeIcon />
-                                        </TimelineDot>
-                                        <TimelineConnector />
-                                    </TimelineSeparator>                                
-                                    <TimelineContent sx={{ py: "15px", px: 2 }}>
-                                    <Typography variant="p" component="span">
-                                        Jan 2023 - Present
-                                    </Typography> <br />
-                                    <Typography  fontWeight="bold" variant="h5" component="span">
-                                        Technical Writer <Typography variant="h5" color="secondary">@Medium Blog</Typography>
-                                    </Typography>
-                                    <ul>
-                                        <li>
-                                            <Typography>
-                                                Authored a technical blog on software engineering, producing 33 articles covering topics such as data structures &
-                                                algorithms, technologies & frameworks, and essential skills which received 2600 views and 178 followers to date.        
-                                            </Typography>
-                                        </li>
-                                    </ul>                                          
-                                    </TimelineContent>
-                                </TimelineItem>
-                                <TimelineItem>
-                                    <TimelineSeparator>
-                                        <TimelineConnector  />
-                                        <TimelineDot color="secondary">
-                                            <CodeIcon />
-                                        </TimelineDot>
-                                        <TimelineConnector />
-                                    </TimelineSeparator>
-                                    <TimelineContent sx={{ py: "15px", px: 2 }}>
-                                    <Typography variant="p" component="span">
-                                        Dec 2018 - Jan 2021
-                                    </Typography> <br />
-                                    <Typography  fontWeight="bold" variant="h5" component="span">
-                                        Pilot Trainee <Typography variant="h5" color="secondary">@Republic Of Singapore Air Force</Typography>
-                                    </Typography>
-                                    <ul>
-                                        <li><Typography> Represented Singapore as a pilot-in-training overseas in a fast-paced, high-stress environment alongside the Royal Australian Air Force for 7 months and attained 44 flying hours flying two aircraft, BAE Systems CT4/B and Pilatus PC-21.</Typography></li>
-                                        <li><Typography> Maintained smooth daily operations of squadron. Ensured all ops duties are carried out efficiently and accurately.</Typography></li>
-                                    </ul>
-                                    </TimelineContent>
-                                </TimelineItem>
-                            </Timeline>
-                        </div>
-                        
-                    </Col>
-                </Row>
-            </Container>
-        </section>
-      )
+        return (
+            <section className="experiences" id="experiences">
+                <Container>
+                    <Row>
+                        <Col>
+                            <div>
+                            <h2 className="m-5" style={{ fontSize: '45px', fontWeight: '700', textAlign: 'center' }}>
+                                Experiences
+                            </h2>
+                                <Timeline 
+                                    sx={{
+                                        [`& .${timelineItemClasses.root}:before`]: {
+                                            flex: 0,
+                                            padding: 0,
+                                        },
+                                    }}
+                                >
+                                    <TimelineItem>
+                                        <TimelineSeparator>
+                                            <TimelineConnector  />
+                                            <TimelineDot color="secondary">
+                                                <CodeIcon />
+                                            </TimelineDot>
+                                            <TimelineConnector />
+                                        </TimelineSeparator>
+                                        <TimelineContent sx={{ py: "15px", px: 2 }}>
+                                        <Typography variant="p" component="span">
+                                            Feb 2023 - Present
+                                        </Typography> <br />
+                                        <Typography  fontWeight="bold" variant="h5" component="span">
+                                            Software Engineer <Typography variant="h5" color="secondary">@Asterisk Technologies</Typography>
+                                        </Typography>
+                                        <Typography></Typography> {/* TODO: add description in list */}
+                                        <div className="item-exp mt-1">
+                                            <i class="devicon-bootstrap-plain colored"></i>
+                                            <i class="devicon-sass-plain colored"></i>
+                                            <i class="devicon-react-original colored"></i>
+                                            <i class="devicon-express-original"></i>
+                                            <i class="devicon-nodejs-plain colored"></i>
+                                            {isMobile && <br />}
+                                            <i class="devicon-electron-original colored"></i>
+                                            <i class="devicon-jest-plain colored"></i>
+                                            <i class="devicon-eslint-plain colored"></i>
+                                            <i class="devicon-mongodb-plain colored"></i>
+                                        </div>
+                                        </TimelineContent>
+                                    </TimelineItem>
+                                    <TimelineItem>
+                                        <TimelineSeparator>
+                                            <TimelineConnector  />
+                                            <TimelineDot color="secondary">
+                                                <CodeIcon />
+                                            </TimelineDot>
+                                            <TimelineConnector />
+                                        </TimelineSeparator>                                
+                                        <TimelineContent sx={{ py: "15px", px: 2 }}>
+                                        <Typography variant="p" component="span">
+                                            Jan 2023 - Present
+                                        </Typography> <br />
+                                        <Typography  fontWeight="bold" variant="h5" component="span">
+                                            Technical Writer <Typography variant="h5" color="secondary">@Medium Blog</Typography>
+                                        </Typography>
+                                        <ul>
+                                            <li>
+                                                <Typography>
+                                                    Authored a technical blog on software engineering, producing 33 articles covering topics such as data structures &
+                                                    algorithms, technologies & frameworks, and essential skills which received 2600 views and 178 followers to date.        
+                                                </Typography>
+                                            </li>
+                                        </ul>                                          
+                                        </TimelineContent>
+                                    </TimelineItem>
+                                    <TimelineItem>
+                                        <TimelineSeparator>
+                                            <TimelineConnector  />
+                                            <TimelineDot color="secondary">
+                                                <CodeIcon />
+                                            </TimelineDot>
+                                            <TimelineConnector />
+                                        </TimelineSeparator>
+                                        <TimelineContent sx={{ py: "15px", px: 2 }}>
+                                        <Typography variant="p" component="span">
+                                            Dec 2018 - Jan 2021
+                                        </Typography> <br />
+                                        <Typography  fontWeight="bold" variant="h5" component="span">
+                                            Pilot Trainee <Typography variant="h5" color="secondary">@Republic Of Singapore Air Force</Typography>
+                                        </Typography>
+                                        <ul>
+                                            <li><Typography> Represented Singapore as a pilot-in-training overseas in a fast-paced, high-stress environment alongside the Royal Australian Air Force for 7 months and attained 44 flying hours flying two aircraft, BAE Systems CT4/B and Pilatus PC-21.</Typography></li>
+                                            <li><Typography> Maintained smooth daily operations of squadron. Ensured all ops duties are carried out efficiently and accurately.</Typography></li>
+                                        </ul>
+                                        </TimelineContent>
+                                    </TimelineItem>
+                                </Timeline>
+                            </div>
+                            
+                        </Col>
+                    </Row>
+                </Container>
+            </section>
+        )
 }
